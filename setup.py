122e4be4e272f4a80602efcc64bd415e462f4ef5
Wayne Moorefield <wayne.moorefield@gmail.com>
2021-10-24 00:24:46 -0400

    redpanda: Packaging work

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='redpanda',
+      version='0.1.0',
+      description='Red Panda Game Framework',
+      author='Wayne Moorefield',
+      author_email='wayne.moorefield@gmail.com',
+      packages=['redpanda']
+)
+
